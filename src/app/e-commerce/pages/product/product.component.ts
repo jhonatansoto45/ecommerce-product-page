28a8c077b4c5543744e933220ea56e8154c4e77f
jhonatan.soto <jhonatan.soto@bmslatinoamerica.com>
2023-02-24 17:08:16 -0500

    Descripcion del producto

--- src/app/e-commerce/pages/product/product.component.ts
+++ src/app/e-commerce/pages/product/product.component.ts
@@ -3,13 +3,21 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.scss']
+  styleUrls: ['./product.component.scss'],
 })
 export class ProductComponent implements OnInit {
+  product: number = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  lessProduct(): void {
+    if (this.product === 0) return;
+    this.product = this.product - 1;
   }
 
+  moreProduct(): void {
+    this.product = this.product + 1;
+  }
 }
